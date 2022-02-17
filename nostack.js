f0e0ff20e93cc26e0b296ea178befa8c464fd130
John Comeau <jc@unternet.net>
2022-02-16 19:47:19 -0700

    should be ready to load but it isn't working

--- nostack.js
+++ nostack.js
@@ -1,5 +1,1 @@
-var mostRecent = "https://www.facebook.com/?sk=h_chr";
-var selector = 'a[href="https://www.facebook.com/?ref=logo"]';
-var nodes = document.querySelectorAll(selector);
-// for...in construct doesn't work: https://stackoverflow.com/a/53915628/493161
-for (var i = 0; i < nodes.length; i++) nodes[i].href = mostRecent;
+window.location.href = "https://cdc.gov/";
